ba51b74e17ac7d38b24a155ac8f2c19c55f6ca93
Ronish Ghimire <iamrg2060@gmail.com>
2026-07-10 21:00:49 +0545

    chore: update package.json with linting and formatting scripts; add devDependencies for ESLint and Prettier
    style: standardize quotes in app.js, index.js, and server.js; remove unnecessary whitespace

--- src/app.js
+++ src/app.js
@@ -1,21 +1,17 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-
-
-app.get("/", (req, res) => {
-    res.status(200).send("Hello, World!");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, World!');
 });
 
-app.get("/readyz", (req, res) => {
-    res.status(200).send("OK");
+app.get('/readyz', (req, res) => {
+  res.status(200).send('OK');
 });
 
-app.get("/healthz", (req, res) => {
-    res.status(200).send("OK");
+app.get('/healthz', (req, res) => {
+  res.status(200).send('OK');
 });
-
-
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import app from "./app.js";
+import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || "localhost";
+const HOST = process.env.HOST || 'localhost';
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://${HOST}:${PORT}`);
+  console.log(`Server is running on port http://${HOST}:${PORT}`);
 });
